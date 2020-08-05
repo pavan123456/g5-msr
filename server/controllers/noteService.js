2cefe1ca27b2a05ce5f5d527486206c982804454
David Miller <david.miller@getg5.com>
2020-08-05 15:18:27 -0700

    login routes

--- server/controllers/noteService.js
+++ server/controllers/noteService.js
@@ -1,19 +1,21 @@
 const axios = require('axios')
+
 const {
   NOTE_SERVICE_USERNAME: username,
   NOTE_SERVICE_PASSWORD: password,
-  NOTE_SERVICE_URL: noteServuceUrl,
+  NOTE_SERVICE_URL: noteServiceUrl,
   NOTE_SERVICE_TOKEN: token
 } = process.env
+
 module.exports = {
 login,
 getNotes
 }
 
 function login () {
-  return axios.post(`${noteServuceUrl}/api/v1/login`, { username, password })
+  return axios.post(`${noteServiceUrl}/api/v1/login`, { username, password })
 }
 
 function getNotes(clientUrn) {
-  return axios.get(`${noteServuceUrl}/api/v1/notes?access_token=${token}&clientUrn=${clientUrn}`)
+  return axios.get(`${noteServiceUrl}/api/v1/notes?access_token=${token}&clientUrn=${clientUrn}`)
 }
--- server/routes/api/report.js
+++ server/routes/api/report.js
@@ -1,6 +1,16 @@
 const notesService = require('../../controllers/noteService')
 
 module.exports = (app) => {
+  app.get('/api/v1/login', async (req, res) => {
+    try {
+      const token = await notesService.login()
+      console.log({ token })
+      res.status(201)
+    } catch (err) {
+      res.status(503).send(err)
+    }
+  })
+
   app.get('/api/v1/report/:clientUrn', async (req, res) => {
     const notes = await notesService.getNotes(req.params.clientUrn)
     res.json(notes.data)
