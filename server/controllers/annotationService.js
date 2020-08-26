a8e43229375c77c4d252689b4db6035a9145f0ae
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-08-26 15:03:27 -0700

    add document this

--- server/controllers/annotationService.js
+++ server/controllers/annotationService.js
@@ -10,31 +10,65 @@ class AnnotationService {
     this.apiToken = null,
     this.tokenIssueDate = null
   }
-
-  login () {
+/**
+ * Login and set the api token
+ *
+ * @returns
+ * @memberof AnnotationService
+ */
+login () {
     return axios.post(`${noteServiceUrl}/api/v1/login`, { username, password })
       .then(({ data }) => {
         this.apiToken = data.token
         this.tokenIssueDate = Date.now()
       })
   }
-  
+  /**
+   * Get all notes for a period and client urn
+   *
+   * @param {*} clientUrn
+   * @param {string} [to='2020-08-07']
+   * @param {string} [from='2020-06-01']
+   * @returns
+   * @memberof AnnotationService
+   */
   async getNotes (clientUrn, to = '2020-08-07', from = '2020-06-01') {
     await this.checkTokenAge()
     return axios.get(`${noteServiceUrl}/api/v1/notes?access_token=${this.apiToken}&clientUrn=${clientUrn}&searchBy=createdAt&to=${to}&from=${from}`)
   }
-
-  async getCases(clientUrn, to = '2020-08-07', from = '2020-06-01') {
+/**
+ * Get all cases for a period and client urn
+ *
+ * @param {*} clientUrn
+ * @param {string} [to='2020-08-07']
+ * @param {string} [from='2020-06-01']
+ * @returns
+ * @memberof AnnotationService
+ */
+async getCases(clientUrn, to = '2020-08-07', from = '2020-06-01') {
     await this.checkTokenAge()
     return axios.get(`${noteServiceUrl}/api/v1/cases?access_token=${this.apiToken}&clientUrn=${clientUrn}&searchBy=closedDate&to=${to}&from=${from}`)
   }
-
-  async getWorkQ(clientUrn, to = '2020-08-07', from = '2020-06-01') {
+/**
+ * Get all workQ items for a period and client urn
+ *
+ * @param {*} clientUrn
+ * @param {string} [to='2020-08-07']
+ * @param {string} [from='2020-06-01']
+ * @returns
+ * @memberof AnnotationService
+ */
+async getWorkQ(clientUrn, to = '2020-08-07', from = '2020-06-01') {
     await this.checkTokenAge()
     return axios.get(`${noteServiceUrl}/api/v1/dam/workq/${clientUrn}?access_token=${this.apiToken}&searchBy=created_at&to=${to}&from=${from}`)
   }
-
-  async getCategories() {
+/**
+ *
+ *
+ * @returns
+ * @memberof AnnotationService
+ */
+async getCategories() {
     await this.checkTokenAge()
     return axios.get(`${noteServiceUrl}/api/v1/categories?access_token=${this.apiToken}`)
   }
