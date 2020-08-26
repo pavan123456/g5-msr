644d890694893f6ff4bf3c5a0867f1e8e80d6f6b
David Miller <david.miller@getg5.com>
2020-08-26 13:38:43 -0700

    structure

--- components/breadcrumb-nav.vue
+++ components/breadcrumb-nav.vue
@@ -1,7 +1,48 @@
 <template>
-  <b-navbar>
-    <b-navbar-brand />
-    <b-breadcrumb :items="items" class="h2 mb-0" />
+  <b-navbar fixed="top" class="d-block px-0 bg-white">
+    <div class="w-100 d-flex mb-2 px-3 justify-content-between">
+      <b-navbar-brand>
+        <b-img-lazy src="/g5-primary-logo.png" height="50" />
+        MSR
+      </b-navbar-brand>
+      <b-nav-text class="align-self-start p-0">
+        <h1 class="mb-0">
+          StorQuest Self Storage
+        </h1>
+        <div class="text-muted text-uppercase small mb-0">
+          From
+          <b-badge class="px-3" style="font-size: 0.75rem;" variant="pale">
+            {{ $route.query.from }}
+          </b-badge>
+          To
+          <b-badge class="px-3" style="font-size: 0.75rem;" variant="pale">
+            {{ $route.query.to }}
+          </b-badge>
+        </div>
+      </b-nav-text>
+      <b-list-group class="flex-row align-items-center">
+        <b-list-group-item
+          v-for="(item, i) in items"
+          :key="item.text"
+          class="p-0 border-0"
+        >
+          <b-btn
+            :href="item.href"
+            variant="transparent"
+            class="p-o m-0 text-uppercase text-muted"
+            @click="move(i)"
+          >
+            {{ item.text }}
+          </b-btn>
+        </b-list-group-item>
+      </b-list-group>
+    </div>
+    <b-progress
+      :max="items.length - 1"
+      :value="position"
+      class="w-100 mb-0 rounded-0"
+      height="3px"
+    />
   </b-navbar>
 </template>
 
@@ -19,10 +60,22 @@ export default {
         ]
       }
     }
+  },
+  data() {
+    return {
+      position: 0
+    }
+  },
+  methods: {
+    move(index) {
+      if (index < 0) {
+        this.position = 0
+      } else if (index > this.items.length - 1) {
+        this.position = this.items.length - 1
+      } else {
+        this.position = index
+      }
+    }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
