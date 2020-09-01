519995f234813f0d5a846a1fb9793645b81b265b
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-09-01 13:43:07 -0700

    bulk update

--- server/routes/api/notes.js
+++ server/routes/api/notes.js
@@ -1,9 +1,14 @@
 const models = require('../../models')
-
+const notesService = require('../../controllers/annotationService')
+const objectUtil = require('../../controllers/util')
 module.exports = (app) => {
   app.put('/api/v1/update', async (req, res) => {
     const { body } = req
-    console.log(JSON.stringify(body))
+    for (let i = 0; i < body.rows.length; i++) {
+      const { id } = body.rows[i]
+      const { keep, discard: update } = objectUtil.split(body.rows[i], ['id'])
+      await notesService.updateNote(id, update)
+    }
     res.json({ stuff: 'all good' })
   })
 }
