badf962abdaa34acbe3e61b5fcb606036a4971fa
David Miller <david.miller@getg5.com>
2020-09-02 09:03:28 -0700

    better table

--- pages/report/index.vue
+++ pages/report/index.vue
@@ -1,13 +1,21 @@
 <template>
-  <b-container>
-    Dashboard-style Report Browser
-    <b-table :items="items">
-      <template v-slot:cell(reportId)="{ item }">
-        <b-btn :to="`/report/${item.reportId}?team=da`">
-          {{ item.reportId }}
-        </b-btn>
-      </template>
-    </b-table>
+  <b-container fluid>
+    <b-row>
+      <b-col>
+        <b-table :items="items">
+          <template v-slot:cell(reportId)="{ item }">
+            <b-btn :to="`/report/${item.reportId}?team=da`" block variant="quaternary-3">
+              View Report
+            </b-btn>
+          </template>
+          <template v-slot:cell(workQ)="{ item }">
+            <h1>
+              {{ item.workQ.length }}
+            </h1>
+          </template>
+        </b-table>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
