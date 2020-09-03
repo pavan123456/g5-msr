1260891087035856c8aafacf77a19f11abe7100e
David Miller <david.miller@getg5.com>
2020-09-03 15:27:08 -0700

    more browsing

--- pages/edit/index.vue
+++ pages/edit/index.vue
@@ -2,12 +2,17 @@
   <b-container fluid>
     <b-row>
       <b-col>
-        <b-table :items="items">
+        <b-table :items="items" :fields="fields">
           <template v-slot:cell(reportId)="{ item }">
-            <b-btn :to="`/edit/${item.reportId}?team=da`" block variant="quaternary-3">
+            <b-btn
+              :to="`/edit/${item.reportId}?team=da`"
+              block
+              variant="quaternary-3"
+              size="sm"
+            >
               Edit Report
             </b-btn>
-            <b-btn :to="`/report/${item.reportId}?edit=false`" block variant="tertiary-2">
+            <b-btn :to="`/report/${item.reportId}?edit=false`" block variant="tertiary-2" size="sm">
               View Report
             </b-btn>
           </template>
@@ -25,8 +30,13 @@
 <script>
 export default {
   async asyncData({ $axios }) {
+    const items = await $axios.$get('api/v1/reports')
     return {
-      items: await $axios.$get('api/v1/reports')
+      items,
+      fields: Object.keys(items[0]).map(field => ({
+        key: field,
+        sortable: true
+      }))
     }
   }
 }
