ffdb21a4f354352b4a65fa0990b43149acbc6c37
David Miller <15317284+pcktbot@users.noreply.github.com>
2021-06-23 11:25:12 -0700

    update linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,9 @@ module.exports = {
     '@nuxtjs',
     'plugin:nuxt/recommended'
   ],
-  // add your custom rules here
   rules: {
-    'nuxt/no-cjs-in-config': 'off',
-    'space-before-function-paren': 'off'
+    'nuxt/no-cjs-in-config': 0,
+    curly: 0,
+    camelcase: ['warn', { properties: 'never', ignoreDestructuring: true }]
   }
 }
