379725f60341c15b8c2fa406a447f93604386ab7
David Miller <15317284+pcktbot@users.noreply.github.com>
2021-06-24 10:38:27 -0700

    back to working client select

--- server/routes/api/hub.js
+++ server/routes/api/hub.js
@@ -3,6 +3,14 @@ const models = require('../../models')
 module.exports = (app) => {
   app.get('/api/v1/hub/clients', async (req, res) => {
     const clients = await models.g5_updatable_client.findAll({
+      where: {
+        properties: {
+          status: {
+            [models.Sequelize.Op.not]: 'Deleted'
+          },
+          g5_internal: false
+        }
+      },
       attributes: [
         'name',
         'urn',
