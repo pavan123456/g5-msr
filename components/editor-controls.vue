d6ff6756b4cc8f998ae035ce1340872907458c7d
David Miller <15317284+pcktbot@users.noreply.github.com>
2021-06-30 14:21:21 -0700

    finished editor UI

--- components/editor-controls.vue
+++ components/editor-controls.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="d-flex align-items-center">
     <b-input-group
-      class="d-flex flex-nowrap align-items-center mr-2"
+      class="d-flex flex-nowrap align-items-center mr-3"
       style="max-width: 400px;"
     >
-      <b-input-group-prepend class="font-weight-bold px-2 text-primary-70">
+      <b-input-group-prepend class="px-2 text-primary-70">
         Client
       </b-input-group-prepend>
       <vue-multiselect
         :value="client"
         :options="clients"
-        style="border-radius: 11px;"
         placeholder="Search"
         track-by="urn"
         label="name"
         @input="onUpdate({ key: 'client', value: $event })"
       >
+        <template #single-label="{ props }">
+          {{ props.name }}
+        </template>
         <template #option="{ option }">
           <b>
             {{ option.name }}
@@ -38,21 +40,25 @@
         @input="onUpdate({ key: 'mode', value: $event })"
       />
       <b-form-select
-        :options="[]"
+        :value="period"
+        :options="periods"
         class="mx-1"
         style="border-radius: 11px; max-width: 150px;"
+        @input="onUpdate({ key: 'period', value: $event })"
       />
       <b-form-select
-        :options="['2021', '2020']"
+        :value="year"
+        :options="years"
         style="border-radius: 11px; max-width: 150px;"
+        @input="onUpdate({ key: 'year', value: $event })"
       />
       <b-input-group-append>
         <b-btn
           :disabled="!isBareMinimum"
           size="sm"
           variant="quaternary-40"
-          class="ml-2"
-          style="border-radius: 50%; align-self: center;"
+          class="ml-2 py-2 px-3 font-weight-bold"
+          style="border-radius: 13px; align-self: center;"
         >
           GO
           <!-- <b-icon-arrow-clockwise shift-h="0" shift-v="-1" /> -->
@@ -67,6 +73,7 @@
         :options="teams"
         buttons
         size="sm"
+        class="spaced-btn"
         button-variant="outline-quaternary-10"
         @input="onUpdate({ key: 'team', value: $event })"
       />
@@ -83,11 +90,9 @@
       <template #button-content>
         <b-icon-three-dots-vertical />
       </template>
-      <b-dropdown-form>
-        <b-btn variant="quaternary-10">
-          Save Report and Share
-        </b-btn>
-      </b-dropdown-form>
+      <b-dropdown-text>
+        Previously Shared Reports will appear here
+      </b-dropdown-text>
     </b-dropdown>
   </div>
 </template>
@@ -102,13 +107,13 @@ export default {
     mode () { return this.$store.state.inputs.mode },
     modes () { return this.$store.state.inputs.modes },
     months () { return this.$store.state.inputs.months },
+    period () { return this.$store.state.inputs.period },
     periods () { return this.$store.state.inputs.periods },
     monthly () { return this.$store.getters.inputs.monthly },
+    year () { return this.$store.state.inputs.year },
+    years () { return this.$store.state.inputs.years },
     isBareMinimum () {
       return this.client !== null
-    },
-    rangePicker () {
-      return this.monthly ? this.months : this.periods
     }
   },
   methods: {
@@ -120,5 +125,15 @@ export default {
 </script>
 
 <style lang="scss">
-
+.spaced-btn > label {
+  margin-right: 0.5rem;
+  border-radius: 13px !important;
+  padding: 0.35rem 0.5rem !important;
+}
+.dropdown-menu {
+  min-width: 30rem;
+  border-color: var(--quaternary-20);
+  border-radius: 13px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+}
 </style>
