93271bc189b48c20c7feeac3f2826b444a90728a
Gabriel Delgado <gabriel.delgado@encora.com>
2022-10-28 02:01:46 -0400

    Updated query to get duplicates in removeDuplicateUpdatable job

--- server/controllers/jobs/removeDuplicateUpdatable.js
+++ server/controllers/jobs/removeDuplicateUpdatable.js
@@ -2,18 +2,18 @@ const models = require('./../../models')
 
 module.exports = async (job, done) => {
   try {
-    const locations = await models.g5_updatable_location.findAll({
-      attributes: [
-        'urn',
-        [models.sequelize.fn('COUNT', models.sequelize.col('urn')), 'count']
-      ],
-      group: ['g5_updatable_location.urn'],
-      having: models.sequelize.where(models.sequelize.fn('COUNT', models.sequelize.col('urn')), '>', 1)
+    const locations = await models.sequelize.query(`
+      SELECT "g5_updatable_location"."urn", COUNT("g5_updatable_location"."urn") AS "count" 
+      FROM "g5_updatable_locations" AS "g5_updatable_location"
+      INNER JOIN "g5_updatable_clients" on "g5_updatable_location"."client_urn" = "g5_updatable_clients"."urn"
+      WHERE "g5_updatable_clients"."properties" ->> 'g5_internal' = 'false'
+      GROUP BY "g5_updatable_location"."urn" 
+      HAVING COUNT("g5_updatable_location"."urn") > 1;`, {
+      type: models.sequelize.QueryTypes.SELECT
     })
 
     for (const location of locations) {
-      const client = location
-      const { urn, count } = client.dataValues
+      const { urn, count } = location
       const dupes = await models.g5_updatable_location.findAll({
         where: { urn },
         limit: count - 1,
